7ce9350e61f17d6952a689b3fcf1f852851cf559
xiaofan yu <xiaofan.yu.22@ucl.ac.uk>
2022-11-18 11:17:18 +0000

    open csv files

--- main_from_file.py
+++ main_from_file.py
@@ -1,18 +1,28 @@
 
-file_address='C:/Users/XIAOYU/Desktop/polygon.csv'
-def read_file(file_address):
-    with open(file_address) as f:
+polygon_file_address='C:/Users/XIAOYU/Desktop/polygon.csv'
+def read_file(polygon_file_address):
+    with open(polygon_file_address) as f:
         data = f.read().splitlines()
-    point_list=[]
+    point_list_a=[]
     for row in data:
-        point_list.append(row.split(','))
-    return point_list
+        point_list_a.append(row.split(','))
+    return point_list_a
 
-print(read_file(file_address))
+print(read_file(polygon_file_address))
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+point_file_address= 'C:/Users/XIAOYU/Desktop/input.csv'
+def read_file(point_file_address):
+    with open(point_file_address) as f:
+        data = f.read().splitlines()
+    point_list_b=[]
+    for row in data:
+        point_list_b.append(row.split(','))
+    return point_list_b
+print(read_file(point_file_address))
 
 
 
